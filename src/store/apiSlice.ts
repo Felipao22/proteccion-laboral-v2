9c6ff9c22b5ac177debe70cb214e0376b50c41c7
Felipao22 <felipeaviani@gmail.com>
2025-11-11 20:14:26 -0300

    feat: apiSlice redux

---
+++ src/store/apiSlice.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const tagTypes = ["User", "Constancia-visita", "File", "Kinds"];
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.REACT_APP_API_BASE_URL,
+  credentials: "include",
+  prepareHeaders: (headers) => {
+    return headers;
+  },
+});
+
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes,
+  endpoints: () => ({}),
+});
