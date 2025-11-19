3f4cdf9d1c291dda81d0a660262fd23836b21fc7
Felipao22 <felipeaviani@gmail.com>
2025-11-19 18:53:49 -0300

    feat: agrego ruta de servicios en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import { LayOut } from "./layout";
 import { HomePage } from "./pages/(public)/HomePage";
 import { LoginPage } from "./pages/(public)/LoginPage";
+import { ServicesPage } from "./pages/(public)/ServicesPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LayOut />}>
           <Route index element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/servicios" element={<ServicesPage />} />
         </Route>
       </Routes>
     </>
