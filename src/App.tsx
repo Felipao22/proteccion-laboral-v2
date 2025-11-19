1485344ec024c7cae88d2ed8e15d31b87d2a94a5
Felipao22 <felipeaviani@gmail.com>
2025-11-19 19:24:03 -0300

    feat: agrego pagina de nosotros en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { LayOut } from "./layout";
 import { HomePage } from "./pages/(public)/HomePage";
 import { LoginPage } from "./pages/(public)/LoginPage";
 import { ServicesPage } from "./pages/(public)/ServicesPage";
+import { NosotrosPage } from "./pages/(public)/NosotrosPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/servicios" element={<ServicesPage />} />
+          <Route path="/nosotros" element={<NosotrosPage />} />
         </Route>
       </Routes>
     </>
