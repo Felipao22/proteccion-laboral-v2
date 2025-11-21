d6c84c22cb74544ce9353437ad4e2038c59fea76
Felipao22 <felipeaviani@gmail.com>
2025-11-20 21:33:10 -0300

    feat: agrego pagina de iniciar sesión y notificaciones para usarlas en toda la App

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,31 @@ import { LoginPage } from "./pages/(public)/LoginPage";
 import { ServicesPage } from "./pages/(public)/ServicesPage";
 import { NosotrosPage } from "./pages/(public)/NosotrosPage";
 import { ContactoPage } from "./pages/(public)/ContactoPage";
+import { DashboardPage } from "./pages/(private)/DashboardPage";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Routes>
         <Route path="/" element={<LayOut />}>
           <Route index element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/iniciar-sesion" element={<LoginPage />} />
           <Route path="/servicios" element={<ServicesPage />} />
           <Route path="/nosotros" element={<NosotrosPage />} />
           <Route path="/contacto" element={<ContactoPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
         </Route>
       </Routes>
     </>
