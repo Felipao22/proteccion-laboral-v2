c3e05d7bf225671b8592d2c4942cc59b6b421aed
Felipao22 <felipeaviani@gmail.com>
2025-12-02 20:17:37 -0300

    feat: agrego ruta para obtener las empresas

--- src/services/usuariosApi.ts
+++ src/services/usuariosApi.ts
@@ -36,14 +36,46 @@ export interface File {
   userEmail: string;
 }
 
+export interface BranchesResponse {
+  message: string;
+  data: Branches[];
+  pagination: Pagination;
+}
+
+export interface Branches {
+  userId: string;
+  email: string;
+  nombreEmpresa: string;
+  nombreSede: string;
+  cuit: string;
+  ciudad: string;
+  direccion: string;
+  telefono: string;
+  emails: any[];
+  accessUser: any[];
+  emailJefe: string;
+  active: boolean;
+}
+
+export interface Pagination {
+  page: number;
+  limit: number;
+  totalPages: number;
+  total: number;
+}
+
 export const usuariosApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getUsers: builder.query<Users[], void>({
       query: () => `/user`,
       providesTags: ["User"],
     }),
+    getBranches: builder.query<BranchesResponse, number>({
+      query: (page = 1) => `/user/branches?page=${page}`,
+      providesTags: ["User"],
+    }),
   }),
   overrideExisting: true,
 });
 
-export const { useGetUsersQuery } = usuariosApi;
+export const { useGetUsersQuery, useGetBranchesQuery } = usuariosApi;
