07168a502bfe7cfed5cb117e47defd68174ee6dd
Felipao22 <felipeaviani@gmail.com>
2025-12-08 17:03:44 -0300

    feat: envuelvo App con AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import "./index.css";
+import { AuthProvider } from "./context/AuthProvider";
 
 const root = document.getElementById("root")!;
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Provider>
   </BrowserRouter>
 );
